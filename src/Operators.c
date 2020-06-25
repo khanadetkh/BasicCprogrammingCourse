29078b26aff116d8ed274e8640f73b6100c0e31f
Pakbung Nathchanon <35398117+pakbungnatchanon@users.noreply.github.com>
2020-06-25 15:28:08 +0700

    Operators and Expressions

---
+++ src/Operators.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+void main (){
+	int x,y,z; // line3 num1+num2=result
+	//line5-6 input x
+	printf("Enter X value : ");
+	scanf("%d",&x);
+	//line8-9 input y
+	printf("Enter Y value : ");
+	scanf("%d",&y);
+	//line11 operation
+	z = x+y;
+	//line13 display result
+	printf("Summary of X and Y is %d" ,z);
+
+}
---
+++ src/fixed_value.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void main( ) {
+	int x; //variable
+	x = 14.8328; //expression
+	printf("x value is %d", x); //display
+}
